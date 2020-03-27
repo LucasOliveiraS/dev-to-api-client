7a46991abcfe299d7fa7b110397a277d8768708b
Lucas <oliveirasp6@gmail.com>
2020-03-27 19:09:26 -0300

    :-)

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
  const DevTo = require('./cliente');
 
 const api = new DevTo({
-  api_key: "vCY4QFUwZrgw9gu7GvRdndJA"
+  api_key: "XXXXXXXXXX"
 });
 
 api.getArticles({ username: "bearer", page: 1 }).then(data => console.log(data))
